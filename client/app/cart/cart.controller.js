03dce0ce395c959514ca18252fce4182817d939b
Maverick Industries <maverickindustries@Miless-MacBook-Pro.local>
2015-05-12 07:49:23 -0600

    Add more elements to the shopping cart system

---
+++ client/app/cart/cart.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('victoryVinylApp')
+  .controller('CartCtrl', function ($scope) {
+    var _this = this;
+    _this.message = 'Hello';
+  });
--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -5,4 +5,22 @@ angular.module('victoryVinylApp')
     var _this = this;
     
     _this.message = 'Hello';
+
+    _this.products = [
+    	{
+    		id: 1,
+    		name: 'Apple',
+    		price: 1.95
+    	},
+    	{
+    		id: 2,
+    		name: 'Monkey',
+    		price: 599.99
+    	},
+    	{
+    		id: 3,
+    		name: 'MacBook Pro',
+    		price: 2499.00
+    	}
+    ];
   });
